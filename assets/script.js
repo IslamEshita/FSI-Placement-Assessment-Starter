82738b3a9c1e0c9c2bfdfd9835d5d3ac17d8fedc
Eshita Islam <islam.eshita@gmail.com>
2021-11-15 19:40:04 -0800

    Added event handlers for each cookie type

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,70 @@
 console.log('JavaScript code has loaded!')
 
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Eshita Islam" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Gingerbread
 let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
-
+let total = 0
+let qtygb = document.querySelector("#qty-gb")
+let qtycc = document.querySelector("#qty-cc")
+let qtysugar = document.querySelector("#qty-sugar")
+let qtytotal = document.querySelector("#qty-total")
 // Code to update name display 
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!')
-
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+let buttongbup = document.querySelector("#add-gb")
+buttongbup.addEventListener('click',function(){
+    console.log('gb plus button was pressed')
+    gb = gb +1
+    qtygb.textContent = gb
+    total = total + 1
+    qtytotal.textContent = total
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+let buttongbdown = document.querySelector("#minus-gb")
+buttongbdown.addEventListener('click',function(){
+    console.log('gb minus button was pressed')
+    gb = gb - 1
+    qtygb.textContent = gb
+    total = total - 1
+    qtytotal.textContent = total
+})
+
+let buttonccup = document.querySelector("#add-cc")
+buttonccup.addEventListener('click',function(){
+    console.log('cc plus button was pressed')
+    cc = cc +1
+    qtycc.textContent = cc
+    total = total + 1
+    qtytotal.textContent = total
+})
+
+let buttonccdown = document.querySelector("#minus-cc")
+buttonccdown.addEventListener('click',function(){
+    console.log('cc minus button was pressed')
+    cc = cc - 1
+    qtycc.textContent = cc
+    total = total - 1
+    qtytotal.textContent = total
+})
+
+let buttonsugarup = document.querySelector("#add-sugar")
+buttonsugarup.addEventListener('click',function(){
+    console.log('sugar plus button was pressed')
+    sugar = sugar +1
+    qtysugar.textContent = sugar
+    total = total + 1
+    qtytotal.textContent = total
+})
+
+let buttonsugardown = document.querySelector("#minus-sugar")
+buttonsugardown.addEventListener('click',function(){
+    console.log('sugar minus button was pressed')
+    sugar = sugar - 1
+    qtysugar.textContent = sugar
+    total = total - 1
+    qtytotal.textContent = total
+})
